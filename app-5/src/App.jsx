a49f296899710ce3a7e8365ee678300fb44fb247
Chase Shields <cwshields2@gmail.com>
2019-08-26 10:40:04 -0500

    completed

--- app-5/src/App.jsx
+++ app-5/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Image from "./Image";
 
@@ -7,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image myImage={"https://cdn.pixabay.com/photo/2016/02/07/14/10/cat-1184781_960_720.jpg"} />
+        <Image url={"https://cdn.pixabay.com/photo/2016/02/07/14/10/cat-1184781_960_720.jpg"} />
       </div>
     );
   }
